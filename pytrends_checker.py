06da573a6415b63c2eca8910e6919b98cca14641
TheMaaziPan <154355972+TheMaaziPan@users.noreply.github.com>
2025-02-24 16:47:20 +0000

    Update pytrends_checker.py

--- pytrends_checker.py
+++ pytrends_checker.py
@@ -23,7 +23,7 @@ st.markdown(
 
     /* Sidebar styling */
     .sidebar .sidebar-content {
-        background-color: #FFFFFF;
+        background-color: #05262C;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
